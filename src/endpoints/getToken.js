71626b8f08c727a8f34bf60d06166c8bc36fa1ea
udhai-kumar <119875168+udhai-kumar@users.noreply.github.com>
2023-01-27 21:39:04 +0530

    Update getToken.js

--- src/endpoints/getToken.js
+++ src/endpoints/getToken.js
@@ -1,11 +1,11 @@
-
 const endPoint = process.env.REACT_APP_TOKEN_ENDPOINT;
-const room_id = process.env.REACT_APP_ROOM_ID; 
+const room_id = process.env.REACT_APP_ROOM_ID;
 
-const getToken = async ( user_role) => {
-  const response = await fetch(`${endPoint}api/token`, {
+const getToken = async (user_id , user_role) => {
+  const response = await fetch(`${endPoint}api/token`, { 
     method: "POST",
     body: JSON.stringify({
+      user_id,
       role: user_role, //host, teacher, guest, student
       type: "app",
       room_id
